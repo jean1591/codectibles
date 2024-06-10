90fba6f05fcd05909c9cdf04d6f6a53265f82a18
jean <j-rob@live.fr>
2024-06-10 17:54:29 +0200

    feat: init navbar

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,11 @@
+export const Navbar = () => {
+  return (
+    <div className="px-4 md:px-0 py-8 mx-auto max-w-6xl flex justify-between border-b border-slate-300 text-xl font-medium">
+      <div>Code Kingdom 👑</div>
+      <div className="flex items-center justify-end gap-x-8">
+        <div>7 💎</div>
+        <div>Log out 👋🏼</div>
+      </div>
+    </div>
+  );
+};
