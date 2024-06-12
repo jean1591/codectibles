4fc59567178a4731ed20f8bff850029e179fddfe
jean <j-rob@live.fr>
2024-06-12 12:28:30 +0200

    chore: improve responsive design

--- src/app/components/ui/popover.tsx
+++ src/app/components/ui/popover.tsx
@@ -16,7 +16,7 @@ export const Popover = ({
         isPopoverVisible ? "visible" : "hidden",
         selectedCell < 7 ? "top-12 sm:top-20" : "bottom-12 sm:bottom-20",
         selectedCell % 7 === 0 ? "left-8 sm:-left-1/2" : "-left-1/2",
-        selectedCell % 7 === 6 ? "-left-40 sm:-left-1/2" : "-left-1/2",
+        selectedCell % 7 === 6 || selectedCell % 7 === 5 ? "-left-40 sm:-left-1/2" : "-left-1/2",
         "z-50 absolute bg-slate-800 p-4 rounded-lg w-48 border border-slate-300"
       )}
     >
