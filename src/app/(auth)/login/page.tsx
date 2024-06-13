41c93b4515d03af81370e3527ea33366e3366019
jean <j-rob@live.fr>
2024-06-13 18:04:20 +0200

    chore: small fixes

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,7 +5,8 @@ import { PiGithubLogo } from "react-icons/pi";
 
 async function signInWithGithub() {
   const supabase = createClient();
-  const { data, error } = await supabase.auth.signInWithOAuth({
+  
+  await supabase.auth.signInWithOAuth({
     provider: "github",
     options: {
       redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/auth/callback`,
