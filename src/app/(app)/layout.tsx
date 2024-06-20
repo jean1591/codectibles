e912761054a729464d0da833f21ace6299502f9a
jean <j-rob@live.fr>
2024-06-20 15:07:35 +0200

    feat: add small screen navbar

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,5 +1,5 @@
 import { Banner } from "../components/banner";
-import { Navbar } from "./ui/navbar";
+import { Navbar, NavbarSmallScreen } from "./ui/navbar";
 
 export default function Layout({
   children,
@@ -7,16 +7,23 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="my-8 sm:my-16 px-4 sm:px-0 mx-auto max-w-7xl grid grid-cols-7 gap-x-12">
-      <div className="hidden sm:block sm:col-span-1">
-        <Navbar />
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow">
+        <div className="my-4 sm:my-16 px-4 sm:px-0 mx-auto max-w-7xl grid grid-cols-7 gap-x-12">
+          <div className="hidden sm:block sm:col-span-1">
+            <Navbar />
+          </div>
+
+          <div className="col-span-7 sm:col-span-6">
+            <Banner />
+            {children}
+          </div>
+        </div>
       </div>
 
-      <div className="col-span-7 sm:col-span-6">
-        <Banner />
-        {children}
+      <div className="sm:hidden">
+        <NavbarSmallScreen />
       </div>
     </div>
   );
 }
-
--- src/app/(app)/ui/navbar.tsx
+++ src/app/(app)/ui/navbar.tsx
@@ -5,15 +5,15 @@ import Link from "next/link";
 import { classNames } from "@/utils";
 import { usePathname } from "next/navigation";
 
-export const Navbar = () => {
-  const pathname = usePathname()
-  const page = pathname.split('/').at(-1)
+const navigationItems = [
+  { title: "profile", href: "/profile" },
+  { title: "success", href: "/success" },
+  { title: "pets", href: "/pets" },
+];
 
-  const navigationItems = [
-    { title: "profile", href: "/profile" },
-    { title: "success", href: "/success" },
-    { title: "pets", href: "/pets" },
-  ];
+export const Navbar = () => {
+  const pathname = usePathname();
+  const page = pathname.split("/").at(-1);
 
   return (
     <div className="flex flex-col gap-y-2">
@@ -23,7 +23,24 @@ export const Navbar = () => {
           href={href}
           className={classNames(
             page === title ? "bg-slate-800" : "bg-none",
-            "hover:bg-slate-800 px-4 py-2 rounded-lg text-slate-300 text-base font-medium uppercase")}
+            "hover:bg-slate-800 px-4 py-2 rounded-lg text-slate-300 text-base font-medium uppercase"
+          )}
+        >
+          {title}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export const NavbarSmallScreen = () => {
+  return (
+    <div className="grid grid-cols-3 bg-slate-700 py-4">
+      {navigationItems.map(({ title, href }) => (
+        <Link
+          key={title}
+          href={href}
+          className="rounded-lg text-slate-200 text-center text-base font-medium capitalize"
         >
           {title}
         </Link>
