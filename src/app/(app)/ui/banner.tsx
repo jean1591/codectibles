fa6cfb26c3a1acd6fe576e62c19f96eab55386cf
jean <j-rob@live.fr>
2024-06-23 12:14:59 +0200

    feat: display correct username

--- src/app/(app)/ui/banner.tsx
+++ src/app/(app)/ui/banner.tsx
@@ -1,7 +1,19 @@
+"use client"
+
+import { RootState } from "@/app/lib/store/store";
+import { useSelector } from "react-redux";
+
 export const Banner = () => {
+  const {user} = useSelector((state: RootState) => state.user)
+
+  // TODO: display skeleton
+  if (!user) {
+    return <></>;
+  }
+
   return (
     <div className="py-8 flex items-center justify-between text-lg">
-      <p className="font-medium">@josebove</p>
+      <p className="font-medium">{`@${user.username}`}</p>
       <p>⚙️</p>
     </div>
   );
