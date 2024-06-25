9f012aaaab9afb10455cb37106c9ae3afe68dcde
jean <j-rob@live.fr>
2024-06-25 22:01:24 +0200

    feat: add Banner skeleton

--- src/app/(app)/ui/banner.tsx
+++ src/app/(app)/ui/banner.tsx
@@ -2,13 +2,14 @@
 
 import { RootState } from "@/app/lib/store/store";
 import { useSelector } from "react-redux";
+import { Banner as BannerSkeleton } from "./skeleton/banner";
 
 export const Banner = () => {
   const { user } = useSelector((state: RootState) => state.user);
 
   // TODO: display skeleton
   if (!user) {
-    return <></>;
+    return <BannerSkeleton />;
   }
 
   return (
---
+++ src/app/(app)/ui/skeleton/banner.tsx
@@ -0,0 +1,7 @@
+export const Banner = () => {
+   return (
+    <div className="py-8 flex items-center justify-between text-lg">
+      <p className="bg-slate-300 text-slate-300 font-medium">@username</p>
+    </div>
+  );
+};
