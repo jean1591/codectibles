d652f34c53adbce40b2f14df26f4371937848539
jean <j-rob@live.fr>
2024-06-27 11:52:22 +0200

    fix: handle pulse on skeleton

--- src/app/(app)/ui/skeleton/banner.tsx
+++ src/app/(app)/ui/skeleton/banner.tsx
@@ -1,6 +1,6 @@
 export const Banner = () => {
   return (
-    <div className="py-8 flex items-center justify-between text-lg">
+    <div className="py-8 flex items-center justify-between text-lg animate-pulse">
       <p className="bg-slate-300 text-slate-300 font-medium">@username</p>
     </div>
   );
