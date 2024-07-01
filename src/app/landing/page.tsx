8c356bdd453bb2e51119376759b3bb9e0c0effa9
Jean Robertou <j-rob@live.fr>
2024-07-01 21:13:56 +0200

    feat: create features

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,9 +1,13 @@
+import { Features } from "./components/features";
 import { Hero } from "./components/hero";
 
 export default function Landing() {
   return (
     <div className="mt-24">
       <Hero />
+      <div className="mt-24 md:mt-32">
+        <Features />
+      </div>
     </div>
   );
 }
