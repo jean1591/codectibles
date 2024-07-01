1a0e515d2e9982cf7e4c008c051f5cb765c61ee6
Jean Robertou <j-rob@live.fr>
2024-07-01 13:32:29 +0200

    feat: lower legendary threshold

--- src/app/(app)/profile/utils/getRandomEmojis.ts
+++ src/app/(app)/profile/utils/getRandomEmojis.ts
@@ -1,22 +1,26 @@
-import { BaseCollectible, Quality } from "@/app/api/interfaces/collectible"
+import { BaseCollectible, Quality } from "@/app/api/interfaces/collectible";
+
 import { allAnimalEmojis } from "../../collection/utils/collectibles";
 
-const getRandomQuality = (legendaryThreshold: number = 10): Quality => {
-  const num = Math.round(Math.random() * 100)
+const getRandomQuality = (legendaryThreshold: number = 5): Quality => {
+  const rareThreshold = legendaryThreshold + 15;
+  const num = Math.ceil(Math.random() * 100);
 
-  if (num < legendaryThreshold) {
-    return Quality.LEGENDARY
+  if (num <= legendaryThreshold) {
+    return Quality.LEGENDARY;
   }
 
-  if (num < 20 + legendaryThreshold) {
-    return Quality.RARE
+  if (num <= rareThreshold) {
+    return Quality.RARE;
   }
 
-  return Quality.COMMON
-}
+  return Quality.COMMON;
+};
 
-export const getRandomEmojis = (count: number = 3, ): BaseCollectible[] => {
+export const getRandomEmojis = (count: number = 3): BaseCollectible[] => {
   const shuffled = allAnimalEmojis.sort(() => 0.5 - Math.random());
 
-  return shuffled.slice(0, count).map(animal => ({...animal, quality: getRandomQuality()}));
-}
+  return shuffled
+    .slice(0, count)
+    .map((animal) => ({ ...animal, quality: getRandomQuality() }));
+};
