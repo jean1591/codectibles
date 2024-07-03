6756e55297f708caa41bfd16af8312cfbfd37c59
jean <j-rob@live.fr>
2024-07-03 17:31:49 +0200

    feat: update tos and privacy-policy

--- src/app/(public)/landing/privacy-policy/page.tsx
+++ src/app/(public)/landing/privacy-policy/page.tsx
@@ -15,51 +15,123 @@ export default function PrivacyPolicy() {
 
       <div className="mt-12 space-y-8">
         <p>
-          Your privacy is important to us. It is codectibles.fr's policy to
-          respect your privacy regarding any information we may collect from you
-          across our website, https://codectibles.fr, and other sites we own and
-          operate.
+          This Privacy Policy describes the policies of Codectibles, 1, Not your
+          business, Paris 75001, France, email: codectibles@mailo.com, phone:
+          0123456789 on the collection, use and disclosure of your information
+          that we collect when you use our website ( https://www.codectibles.fr
+          ). (the “Service”). By accessing or using the Service, you are
+          consenting to the collection, use and disclosure of your information
+          in accordance with this Privacy Policy. If you do not consent to the
+          same, please do not access or use the Service. We may modify this
+          Privacy Policy at any time without any prior notice to you and will
+          post the revised Privacy Policy on the Service. The revised Policy
+          will be effective 180 days from when the revised Policy is posted in
+          the Service and your continued access or use of the Service after such
+          time will constitute your acceptance of the revised Privacy Policy. We
+          therefore recommend that you periodically review this page.
         </p>
-        <p>
-          We only ask for personal information when we truly need it to provide
-          a service to you. We collect it by fair and lawful means, with your
-          knowledge and consent. We also let you know why we're collecting it
-          and how it will be used.
-        </p>
-        <p>
-          We only retain collected information for as long as necessary to
-          provide you with your requested service. What data we store, we'll
-          protect within commercially acceptable means to prevent loss and
-          theft, as well as unauthorised access, disclosure, copying, use or
-          modification.
-        </p>
-        <p>
-          We don't share any personally identifying information publicly or with
-          third-parties, except when required to by law.
-        </p>
-        <p>
-          We act in the capacity of a data controller and a data processor with
-          regard to the personal data processed through codectibles.fr and the
-          services in terms of the applicable data protection laws, including
-          the EU General Data Protection Regulation (GDPR).
-        </p>
-        <p>
-          Our website may link to external sites that are not operated by us.
-          Please be aware that we have no control over the content and practices
-          of these sites, and cannot accept responsibility or liability for
-          their respective privacsy policies.
-        </p>
-        <p>
-          You are free to refuse our request for your personal information, with
-          the understanding that we may be unable to provide you with some of
-          your desired services.
-        </p>
-        <p>
-          Your continued use of our website will be regarded as acceptance of
-          our practices around privacy and personal information. If you have any
-          questions about how we handle user data and personal information, feel
-          free to contact us.
-        </p>
+
+        <div>
+          <p>1. Information we collect</p>
+          <p>
+            We will collect and process the following personal information about
+            you:
+          </p>
+
+          <ul className="list-disc">
+            <li className="ml-8">Name</li>
+            <li className="ml-8">Email</li>
+            <li className="ml-8">Social Media Profile</li>
+            <li className="ml-8">pull request metadata</li>
+          </ul>
+        </div>
+
+        <div>
+          <p>2. How we use your information</p>
+          <p>
+            We will use the information that we collect about you for the
+            following purposes:
+          </p>
+          <ul className="list-disc">
+            <li className="ml-8">Creating user account</li>
+            <li className="ml-8">Access to application services</li>
+          </ul>
+          <p>
+            If we want to use your information for any other purpose, we will
+            ask you for consent and will use your information only on receiving
+            your consent and then, only for the purpose(s) for which grant
+            consent unless we are required to do otherwise by law.
+          </p>
+        </div>
+
+        <div>
+          <p>3. Retention of your information</p>
+          <p>
+            We will retain your personal information with us for 90 days to 2
+            years after users terminate their accounts or for as long as we need
+            it to fulfill the purposes for which it was collected as detailed in
+            this Privacy Policy. We may need to retain certain information for
+            longer periods such as record-keeping / reporting in accordance with
+            applicable law or for other legitimate reasons like enforcement of
+            legal rights, fraud prevention, etc. Residual anonymous information
+            and aggregate information, neither of which identifies you (directly
+            or indirectly), may be stored indefinitely.
+          </p>
+        </div>
+
+        <div>
+          <p>4. Your rights</p>
+          <p>
+            Depending on the law that applies, you may have a right to access
+            and rectify or erase your personal data or receive a copy of your
+            personal data, restrict or object to the active processing of your
+            data, ask us to share (port) your personal information to another
+            entity, withdraw any consent you provided to us to process your
+            data, a right to lodge a complaint with a statutory authority and
+            such other rights as may be relevant under applicable laws. To
+            exercise these rights, you can write to us at codectibles@mailo.com.
+            We will respond to your request in accordance with applicable law.
+          </p>
+          <p>
+            Do note that if you do not allow us to collect or process the
+            required personal information or withdraw the consent to process the
+            same for the required purposes, you may not be able to access or use
+            the services for which your information was sought.
+          </p>
+        </div>
+
+        <div>
+          <p>5. Cookies etc</p>
+          <p>
+            To learn more about how we use these and your choices in relation to
+            these tracking technologies, please refer to our Cookie Policy.
+          </p>
+        </div>
+
+        <div>
+          <p>6. Security</p>
+          <p>
+            The security of your information is important to us and we will use
+            reasonable security measures to prevent the loss, misuse or
+            unauthorized alteration of your information under our control.
+            However, given the inherent risks, we cannot guarantee absolute
+            security and consequently, we cannot ensure or warrant the security
+            of any information you transmit to us and you do so at your own
+            risk.
+          </p>
+        </div>
+
+        <div>
+          <p>7. Grievance / data protection officer</p>
+          <p>
+            If you have any queries or concerns about the processing of your
+            information that is available with us, you may email our Grievance
+            Officer at codectibles, 1, Not your business, email:
+            codectibles@mailo.com. We will address your concerns in accordance
+            with applicable law.
+          </p>
+        </div>
+
         <p>This policy is effective as of 01 July 2024.</p>
       </div>
     </div>
--- src/app/(public)/landing/tos/page.tsx
+++ src/app/(public)/landing/tos/page.tsx
@@ -15,101 +15,119 @@ export default function ToS() {
 
       <div className="mt-12 space-y-8">
         <div>
-          <p>1. Introduction</p>
+          <p>1. Terms</p>
           <p>
-            By using codectibles.fr you confirm your acceptance of, and agree to
-            be bound by, these terms and conditions.
+            By accessing this Website, accessible from
+            https://www.codectibles.fr, you are agreeing to be bound by these
+            Website Terms and Conditions of Use and agree that you are
+            responsible for the agreement with any applicable local laws. If you
+            disagree with any of these terms, you are prohibited from accessing
+            this site. The materials contained in this Website are protected by
+            copyright and trade mark law.
           </p>
         </div>
 
         <div>
-          <p>2. Agreement to Terms and Conditions</p>
+          <p>2. Use License</p>
           <p>
-            This Agreement takes effect on the date on which you first use the
-            codectibles.fr application.
+            Permission is granted to temporarily download one copy of the
+            materials on https://www.codectibles.fr's Website for personal,
+            non-commercial transitory viewing only. This is the grant of a
+            license, not a transfer of title, and under this license you may
+            not:
+          </p>
+          <ul className="list-disc">
+            <li className="ml-8">modify or copy the materials;</li>
+            <li className="ml-8">
+              use the materials for any commercial purpose or for any public
+              display;
+            </li>
+            <li className="ml-8">
+              attempt to reverse engineer any software contained on
+              https://www.codectibles.fr's Website;
+            </li>
+            <li className="ml-8">
+              remove any copyright or other proprietary notations from the
+              materials; or
+            </li>
+            <li className="ml-8">
+              transferring the materials to another person or "mirror" the
+              materials on any other server.
+            </li>
+          </ul>
+          <p>
+            This will let https://www.codectibles.fr to terminate upon
+            violations of any of these restrictions. Upon termination, your
+            viewing right will also be terminated and you should destroy any
+            downloaded materials in your possession whether it is printed or
+            electronic format.
           </p>
         </div>
 
         <div>
-          <p>3. License Duration</p>
+          <p>3. Disclaimer</p>
           <p>
-            This license is perpetual, with the exception of you breaking any
-            part of this license, in which case you lose all rights under the
-            license.
+            All the materials on https://www.codectibles.fr's Website are
+            provided "as is". https://www.codectibles.fr makes no warranties,
+            may it be expressed or implied, therefore negates all other
+            warranties. Furthermore, https://www.codectibles.fr does not make
+            any representations concerning the accuracy or reliability of the
+            use of the materials on its Website or otherwise relating to such
+            materials or any sites linked to this Website.
           </p>
         </div>
 
         <div>
-          <p>4. Product usage</p>
+          <p>4. Limitations</p>
           <p>
-            By using codectibles.fr, you agree to receive important product
-            updates from codectibles.fr via the email linked with your Google
-            account or the email you used to register your account. You can
-            opt-out of this product updates anytime by clicking to the
-            "Unsubscribe" link at the bottom of each email. We only send
-            important product updates.
+            https://www.codectibles.fr or its suppliers will not be hold
+            accountable for any damages that will arise with the use or
+            inability to use the materials on https://www.codectibles.fr's
+            Website, even if https://www.codectibles.fr or an authorize
+            representative of this Website has been notified, orally or written,
+            of the possibility of such damage. Some jurisdiction does not allow
+            limitations on implied warranties or limitations of liability for
+            incidental damages, these limitations may not apply to you.
           </p>
         </div>
 
         <div>
-          <p>5. Disclaimer</p>
+          <p>5. Revisions and Errata</p>
           <p>
-            It is not warranted that codectibles.fr will meet your requirements
-            or that its operation will be uninterrupted or error free. All
-            express and implied warranties or conditions not stated in this
-            Agreement (including without limitation, loss of profits, loss or
-            corruption of data, business interruption or loss of contracts), so
-            far as such exclusion or disclaimer is permitted under the
-            applicable law are excluded and expressly disclaimed. This Agreement
-            does not affect your statutory rights.
+            The materials appearing on https://www.codectibles.fr's Website may
+            include technical, typographical, or photographic errors.
+            https://www.codectibles.fr will not promise that any of the
+            materials in this Website are accurate, complete, or current.
+            https://www.codectibles.fr may change the materials contained on its
+            Website at any time without notice. https://www.codectibles.fr does
+            not make any commitment to update the materials.
           </p>
         </div>
 
         <div>
-          <p>6. Warranties and Limitation of Liability</p>
+          <p>6. Links</p>
           <p>
-            codectibles.fr does not give any warranty, guarantee or other term
-            as to the quality, fitness for purpose or otherwise of the
-            software.codectibles.fr shall not be liable to you by reason of any
-            representation (unless fraudulent), or any implied warranty,
-            condition or other term, or any duty at common law, for any loss of
-            profit or any indirect, special or consequential loss, damage,
-            costs, expenses or other claims (whether caused by codectibles.fr's
-            negligence or the negligence of its servants or agents or otherwise)
-            which arise out of or in connection with the provision of any goods
-            or services bycodectibles.fr. codectibles.fr shall not be liable or
-            deemed to be in breach of contract by reason of any delay in
-            performing, or failure to perform, any of its obligations if the
-            delay or failure was due to any cause beyond its reasonable control.
-            Notwithstanding contrary clauses in this Agreement, in the event
-            that codectibles.fr are deemed liable to you for breach of this
-            Agreement, you agree thatcodectibles.fr's liability is limited to
-            the amount actually paid by you for your services or software, which
-            amount calculated in reliance upon this clause. You hereby release
-            codectibles.fr from any and all obligations, liabilities and claims
-            in excess of this limitation.
+            https://www.codectibles.fr has not reviewed all of the sites linked
+            to its Website and is not responsible for the contents of any such
+            linked site. The presence of any link does not imply endorsement by
+            https://www.codectibles.fr of the site. The use of any linked
+            website is at the user's own risk.
           </p>
         </div>
 
         <div>
-          <p>7. Responsibilities</p>
+          <p>7. Site Terms of Use Modifications</p>
           <p>
-            codectibles.fr is not responsible for what the user does with the
-            user-generated content.
+            https://www.codectibles.fr may revise these Terms of Use for its
+            Website at any time without prior notice. By using this Website, you
+            are agreeing to be bound by the current version of these Terms and
+            Conditions of Use.
           </p>
         </div>
 
         <div>
-          <p>8. General Terms and Law</p>
-          <p>
-            This Agreement is governed by the laws of France. You acknowledge
-            that no joint venture, partnership, employment, or agency
-            relationship exists between you and codectibles.fr as a result of
-            your use of these services. You agree not to hold yourself out as a
-            representative, agent or employee of codectibles.fr. You agree that
-            codectibles.fr will not be liable by reason of any representation,
-            act or omission to act by you.
-          </p>
+          <p>8. Your Privacy</p>
+          <p>Please read our Privacy Policy.</p>
         </div>
 
         <p>Last updated: 01 July 2024.</p>
